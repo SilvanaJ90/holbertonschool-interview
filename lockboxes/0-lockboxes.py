f3cdbe296e4fad7f92c262f218856ba171aba5f0
SilvanaJ90 <silvanajaramillo90@gmail.com>
2023-03-24 19:57:07 -0500

    create-Lockboxes

---
+++ lockboxes/0-lockboxes.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Method that determines if all the boxes can be opened """
+
+
+def canUnlockAll(boxes):
+    """ Method that determines if all the boxes can be opened """
+    unlocked = [0]
+    n = len(boxes)
+    for i in unlocked:
+        for key in boxes[i]:
+            if key not in unlocked and key < n:
+                unlocked.append(key) 
+    if len(unlocked) == n:
+        return True
+    return False
