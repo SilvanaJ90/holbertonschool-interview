acc61c95fed996999bce182016b3c6ebd04b20ac
SilvanaJ90 <silvanajaramillo90@gmail.com>
2023-03-24 20:06:41 -0500

    create-Lockboxes

--- lockboxes/0-lockboxes.py
+++ lockboxes/0-lockboxes.py
@@ -9,7 +9,7 @@ def canUnlockAll(boxes):
     for i in unlocked:
         for key in boxes[i]:
             if key not in unlocked and key < n:
-                unlocked.append(key) 
+                unlocked.append(key)
     if len(unlocked) == n:
         return True
     return False
