24382c84c08a3ef717a5b2b6e3578590cd042d1c
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-01-24 06:03:18 -0500

    update

---
+++ heap_insert/1-heap_insert.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* Write a function that inserts a value into a Max Binary Heap:
+* @root: is a double pointer to the root node of the Heap
+* @value: is the value to store in the node to be inserted
+* Return: new pointer or NULL if failed
+*/
+
+
+heap_t *heap_insert(heap_t **root, int value);
+heap_insert *new_p = malloc(sizeof(heap_insert))
+new_p ->
