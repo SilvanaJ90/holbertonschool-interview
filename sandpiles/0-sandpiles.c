a4201d1912878706d8ec31c5ddc47dda1c2a55b6
SilvanaJ90 <silvanajaramillo90@gmail.com>
2024-02-02 21:56:06 -0500

    style betty

--- sandpiles/0-sandpiles.c
+++ sandpiles/0-sandpiles.c
@@ -35,53 +35,70 @@ void print_grid(int grid[3][3])
  * @param grid2 The second 3x3 grid to be added to the first grid.
  */
 
-void sandpiles_sum(int grid1[3][3], int grid2[3][3]) {
-    int i, j, unstable;
-    int tmp[3][3];
+void sandpiles_sum(int grid1[3][3], int grid2[3][3])
+{
+	int i, j, unstable;
+	int tmp[3][3];
 
-    for (i = 0; i < 3; i++) {
-        for (j = 0; j < 3; j++) {
-            grid1[i][j] += grid2[i][j];
-        }
-    }
+	/* Perform grid addition */
+	for (i = 0; i < 3; i++)
+	{
+		for (j = 0; j < 3; j++)
+		{
+			grid1[i][j] += grid2[i][j];
+		}
+	}
 
-    do {
-        unstable = 0;
+	/* Stabilize the grid until it becomes stable */
+	do {
+		unstable = 0;
 
-        for (i = 0; i < 3; i++) {
-            for (j = 0; j < 3; j++) {
-                tmp[i][j] = grid1[i][j];
-            }
-        }
+		/* Copy the current state of the grid for comparison */
+		for (i = 0; i < 3; i++)
+		{
+			for (j = 0; j < 3; j++)
+			{
+				tmp[i][j] = grid1[i][j];
+			}
+		}
 
-        for (i = 0; i < 3; i++) {
-            for (j = 0; j < 3; j++) {
-                if (tmp[i][j] > 3) {
-                    unstable = 1;
-                }
-            }
-        }
+		/* Check for instability in the grid */
+		for (i = 0; i < 3; i++)
+		{
+			for (j = 0; j < 3; j++)
+			{
+				if (tmp[i][j] > 3)
+				{
+					unstable = 1;
+				}
+			}
+		}
 
-        if (unstable) {
-            printf("=\n");
-            print_grid(grid1);
-        }
+		/* Print the grid if it is unstable */
+		if (unstable)
+		{
+			printf("=\n");
+			print_grid(grid1);
+		}
 
-
-        for (i = 0; i < 3; i++) {
-            for (j = 0; j < 3; j++) {
-                if (tmp[i][j] > 3) {
-                    grid1[i][j] -= 4;
-                    if (i > 0)
-                        grid1[i - 1][j] += 1;
-                    if (i < 2)
-                        grid1[i + 1][j] += 1;
-                    if (j > 0)
-                        grid1[i][j - 1] += 1;
-                    if (j < 2)
-                        grid1[i][j + 1] += 1;
-                }
-            }
-        }
-    } while (unstable);
+		/* Redistribute excess grains to stabilize the grid */
+		for (i = 0; i < 3; i++)
+		{
+			for (j = 0; j < 3; j++)
+			{
+				if (tmp[i][j] > 3)
+				{
+					grid1[i][j] -= 4;
+					if (i > 0)
+						grid1[i - 1][j] += 1;
+					if (i < 2)
+						grid1[i + 1][j] += 1;
+					if (j > 0)
+						grid1[i][j - 1] += 1;
+					if (j < 2)
+						grid1[i][j + 1] += 1;
+				}
+			}
+		}
+	} while (unstable);
 }
